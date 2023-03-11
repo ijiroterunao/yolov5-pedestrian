26eb4341e96be560633fbabdc20943a6636e4147
ijiroterunao <ijiroterunao@outlook.com>
2023-03-11 20:12:58 +0800

    EIoU更换方式,注意此时注意力模块还没删除

--- utils/metrics.py
+++ utils/metrics.py
@@ -127,7 +127,7 @@ class ConfusionMatrix:
         detections = detections[detections[:, 4] > self.conf]
         gt_classes = labels[:, 0].int()
         detection_classes = detections[:, 5].int()
-        iou = general.box_iou(labels[:, 1:], detections[:, :4])
+        iou = general.bbox_iou(labels[:, 1:], detections[:, :4])
 
         x = torch.where(iou > self.iou_thres)
         if x[0].shape[0]:
